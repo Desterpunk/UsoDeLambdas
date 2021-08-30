7fa95aad0da4eeab9c688a5184bd931153c98129
Jhon Serna <jhonalfaber@gmail.com>
2021-08-30 16:40:18 -0500

    implementando lambda con interface

--- src/main/java/lambdas/LambdaApp.java
+++ src/main/java/lambdas/LambdaApp.java
@@ -17,8 +17,20 @@ public class LambdaApp {
         }
 
     }
+    public void calcular(){
+//        Operacion operacion = new Operacion() {
+//            @Override
+//            public double calcularPromedio(double n1, double n2) {
+//                return (n1+n2)/2;
+//            }
+//        };
+        Operacion operacion = (double n1, double n2) -> (n1 + n2)/2;
+        operacion.calcularPromedio(2,3);
+        System.out.println("o = " + operacion.calcularPromedio(2,3));
+    }
     public static void main(String[] args) {
         LambdaApp app = new LambdaApp();
         app.ordenar();
+        app.calcular();
     }
 }
---
+++ src/main/java/lambdas/Operacion.java
@@ -0,0 +1,5 @@
+package lambdas;
+
+public interface Operacion {
+    double calcularPromedio(double n1, double n2);
+}
