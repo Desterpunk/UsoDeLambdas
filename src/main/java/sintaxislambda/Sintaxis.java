b47644595d68666c6297ca77cfda496a90a0095e
Jhon Serna <jhonalfaber@gmail.com>
2021-08-31 14:31:19 -0500

    Formas de escribir lambda con {} o vacios

--- src/main/java/sintaxislambda/Sintaxis.java
+++ src/main/java/sintaxislambda/Sintaxis.java
@@ -4,7 +4,20 @@ import lambdas.Operacion;
 
 public class Sintaxis {
     public double probarSintaxis(){
-        Operacion operacion = (double x, double y) -> (x + y)/2;
+        //Operacion operacion = (double x, double y) -> (x + y)/2;
+        //return operacion.calcularPromedio(2,3);
+        //Operacion operacion = (double x, double y) -> {return (x + y)/2;};
+
+//        Operacion operacion = (double x, double y) -> {
+//            double a = 2.0;
+//            System.out.println(a);
+//            return (x + y)/2 + a;
+//        };
+
+//        Operacion operacion = (x, y) -> (x + y)/2;
+        Operacion operacion = (double x, double y) -> {
+            return x + y;
+        };
         return operacion.calcularPromedio(2,3);
     }
 
